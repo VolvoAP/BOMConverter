5bbc92dc2a1007afb608a2d20a20b3273a77e40d
Manuelyrs <manuel.yaramis@volvocars.com>
2025-01-15 04:37:20 +0100

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Start the web server with Gunicorn
+gunicorn app:app --workers=1 --threads=4 --timeout 300 &
+
+# Start your secondary Python script
+python "ARO RIPPER HTML.py"
+
+# Wait for both processes to finish
+wait
