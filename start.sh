79e91f2e66294ed16188e5cee1dadd93ea810b49
Manuelyrs <manuel.yaramis@volvocars.com>
2025-02-12 20:17:40 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 gunicorn app:app --workers=1 --threads=4 --timeout 300 &
 
 # Start your secondary Python script
-python "ARO RIPPER HTML.py"
+python "ARO_RIPPER_HTML.py"
 
 # Wait for both processes to finish
 wait
