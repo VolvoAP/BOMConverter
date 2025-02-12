61f01f8d15ecf18a3d50d3d41c08a78fb66ebc3b
Manuelyrs <manuel.yaramis@volvocars.com>
2025-02-12 20:53:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -215,8 +215,5 @@ def download_file():
 
 
 if __name__ == "__main__":
-    app.run(
-        debug=True,
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080)),
-    )
+    port = int(os.environ.get("PORT", 5000))  # Railway gebruikt de dynamische poort
+    app.run(debug=False, host="0.0.0.0", port=port)
