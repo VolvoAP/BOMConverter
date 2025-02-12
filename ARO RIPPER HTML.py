d411505038175d71f635cf072f8616ce396805b8
Manuelyrs <manuel.yaramis@volvocars.com>
2025-02-12 20:09:15 +0100

    Update ARO RIPPER HTML.py

--- ARO RIPPER HTML.py
+++ ARO RIPPER HTML.py
@@ -117,6 +117,6 @@ def download_action():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))  # Gebruik de dynamische poort
+    port = int(os.environ.get("PORT", 8090))  # Gebruik de dynamische poort
     app.run(host="0.0.0.0", port=port, debug=True)
     
