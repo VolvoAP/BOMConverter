db82a0f50f7e5506466cff878eb018b87724d53e
Manuelyrs <manuel.yaramis@volvocars.com>
2025-08-07 17:26:30 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@
 gunicorn app:app --workers=1 --threads=4 --timeout 300 &
 
 # Start your secondary Python script
-python "arorip.py"
+python "psf_dashboard.py"
 
 # Wait for both processes to finish
 wait
+
